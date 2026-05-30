f9de36fae1cebbdca3b71ccc7ed4c8baa04b3379
Cotrog <arstroyan@gmail.com>
2026-05-30 14:34:46 +0300

    Config updates

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,14 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Config struct {
 	DB DBConfig
 	Server ServerConfig
+	RabbitMQ RabbitMQConfig
+	Worker WorkerConfig
 }
 
 type DBConfig struct {
@@ -19,11 +22,25 @@ type DBConfig struct {
 	SSLMode string
 }
 
+type RabbitMQConfig struct {
+	URL string
+	NewOrderQueue string
+	StatusQueue string
+}
+
+type WorkerConfig struct {
+	WorkersCount int
+}
+
 type ServerConfig struct {
 	Port string
 }
 
 func New() (*Config, error) {
+	workerCount, err := strconv.Atoi(os.Getenv("WORKERS_COUNT"))
+	if err != nil || workerCount <= 0 {
+		workerCount = 3 //by default
+	}
 	return &Config{
 		DB: DBConfig{
 			Host: os.Getenv("DB_HOST"),
@@ -35,6 +52,14 @@ func New() (*Config, error) {
 		},
 		Server: ServerConfig{
 			Port: os.Getenv("SERVER_PORT"),
+		},
+		RabbitMQ: RabbitMQConfig{
+			URL: os.Getenv("RABBITMQ_URL"),
+			NewOrderQueue: os.Getenv("RABBITMQ_NEW_ORDERS_QUEUE"),
+			StatusQueue: os.Getenv("RABBITMQ_NEW_ORDERS_QUEUE"),
+		},
+		Worker: WorkerConfig{
+			WorkersCount: workerCount,
 		},
 	}, nil
 }
